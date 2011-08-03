f7449c0b62806856370d204d0a897abbad634258
Homer Strong <homer.strong@gmail.com>
2011-08-03 09:40:32 -0700

    xRevert "tweaked init argument destructuring."
    This reverts commit fb641c06e997bf8b3d2cb67547cc8fb9b9db885a.

--- src/clj_redis/client.clj
+++ src/clj_redis/client.clj
@@ -7,7 +7,7 @@
 (def ^{:private true} local-url
   "redis://127.0.0.1:6379")
 
-(defn init [& {:keys [url timeout test-on-borrow] :as opts}]
+(defn init [& [{:keys [url timeout test-on-borrow] :as opts}]]
   (let [uri (URI. (or url local-url))
         tout (or timeout 2000)
         host (.getHost uri)
