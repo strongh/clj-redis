3a5436bf94a2035bf3dbe4f2a8a91c03b40b032e
Homer Strong <homer.strong@gmail.com>
2011-09-01 12:48:16 -0700

    0.0.14

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.strongh/clj-redis "0.0.13"
+(defproject org.clojars.strongh/clj-redis "0.0.14"
   :description "Clojure Redis client library"
   :url "https://github.com/strongh/clj-redis"
   :dependencies [[org.clojure/clojure "1.3.0-alpha4"]
