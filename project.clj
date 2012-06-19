bdcfeccd01c085383d021692c63f38d0428ef549
Homer Strong <homer.strong@gmail.com>
2012-06-19 14:42:21 -0700

    use clojure 1.4

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.strongh/clj-redis "0.0.19"
+(defproject org.clojars.strongh/clj-redis "0.0.20"
   :description "Clojure Redis client library"
   :url "https://github.com/strongh/clj-redis"
   :dependencies
-    [[org.clojure/clojure "1.3.0-alpha4"]
-     [org.clojars.aperiodic/jedis "2.0.1"]]
+  [[org.clojure/clojure "1.4.0"]
+   [org.clojars.aperiodic/jedis "2.0.1"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
